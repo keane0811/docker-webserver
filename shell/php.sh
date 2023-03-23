59ece172ca1819544a09060db2c841ecff70de88
Keane <jinxin0811@126.com>
2023-03-24 04:58:25 +0800

    mod docker-compose.yml

--- shell/php.sh
+++ shell/php.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
 # vscode设置php可执行文件
-docker exec -i --user=1000:1000 php8 php $@
+docker exec -i --user=1000:1000 php8.1 php $@
--- shell/python.sh
+++ shell/python.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-# vscode设置php可执行文件
-docker exec -it --user=1000:1000 -w /project py3 python $@
+# vscode设置python可执行文件
+docker exec -it --user=1000:1000 py3 python $@
